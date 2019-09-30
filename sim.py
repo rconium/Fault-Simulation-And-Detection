edd256e7980d24f73e0b171b392f861bf0a50d07
deNuru <rremigoso4310@gmail.com>
2019-09-30 16:35:43 -0500

    added the optimized sim.py from richard-sandbox

--- sim.py
+++ sim.py
@@ -221,213 +221,40 @@ def netRead(netName):
 
 
 # -------------------------------------------------------------------------------------------------------------------- #
-# FUNCTION: calculates the output value for each logic gate
-def gateCalc(circuit, node):
-
-    # terminal will contain all the input wires of this logic gate (node)
-    terminals = list(circuit[node][1])  
-    # print("THIS IS THE FUCKING NODES")
-    # print(node)
-    # print(circuit[node])
-    # print(circuit[node][0])
-
-    # print ("THIS IS THE FUCKIING TERMINALS")
-    # print(terminals[0])
-    # print(circuit[terminals[0]])
-    # print("")
-
-    # If the node is an Inverter gate output, solve and return the output
-    if circuit[node][0] == "NOT":
-        if circuit[terminals[0]][3] == '0':
-            circuit[node][3] = '1'
-        elif circuit[terminals[0]][3] == '1':
-            circuit[node][3] = '0'
-        elif circuit[terminals[0]][3] == "U":
-            circuit[node][3] = "U"
-        else:  # Should not be able to come here
-            return -1
-        return circuit
-
-    # If the node is an AND gate output, solve and return the output
-    elif circuit[node][0] == "AND":
-        # Initialize the output to 1
-        circuit[node][3] = '1'
-        # Initialize also a flag that detects a U to false
-        unknownTerm = False  # This will become True if at least one unknown terminal is found
-
-        # if there is a 0 at any input terminal, AND output is 0. If there is an unknown terminal, mark the flag
-        # Otherwise, keep it at 1
-        for term in terminals:  
-            if circuit[term][3] == '0':
-                circuit[node][3] = '0'
-                break
-            if circuit[term][3] == "U":
-                unknownTerm = True
-
-        if unknownTerm:
-            if circuit[node][3] == '1':
-                circuit[node][3] = "U"
-        return circuit
-
-    # If the node is a NAND gate output, solve and return the output
-    elif circuit[node][0] == "NAND":
-        # Initialize the output to 0
-        circuit[node][3] = '0'
-        # Initialize also a variable that detects a U to false
-        unknownTerm = False  # This will become True if at least one unknown terminal is found
-
-        # if there is a 0 terminal, NAND changes the output to 1. If there is an unknown terminal, it
-        # changes to "U" Otherwise, keep it at 0
-        for term in terminals:
-            if circuit[term][3] == '0':
-                circuit[node][3] = '1'
-                break
-            if circuit[term][3] == "U":
-                unknownTerm = True
-                break
-
-        if unknownTerm:
-            if circuit[node][3] == '0':
-                circuit[node][3] = "U"
-        return circuit
-
-    # If the node is an OR gate output, solve and return the output
-    elif circuit[node][0] == "OR":
-        # Initialize the output to 0
-        circuit[node][3] = '0'
-        # Initialize also a variable that detects a U to false
-        unknownTerm = False  # This will become True if at least one unknown terminal is found
-
-        # if there is a 1 terminal, OR changes the output to 1. Otherwise, keep it at 0
-        for term in terminals:
-            if circuit[term][3] == '1':
-                circuit[node][3] = '1'
-                break
-            if circuit[term][3] == "U":
-                unknownTerm = True
-
-        if unknownTerm:
-            if circuit[node][3] == '0':
-                circuit[node][3] = "U"
-        return circuit
-
-    # If the node is an NOR gate output, solve and return the output
-    if circuit[node][0] == "NOR":
-        # Initialize the output to 1
-        circuit[node][3] = '1'
-        # Initialize also a variable that detects a U to false
-        unknownTerm = False  # This will become True if at least one unknown terminal is found
-
-        # if there is a 1 terminal, NOR changes the output to 0. Otherwise, keep it at 1
-        for term in terminals:
-            if circuit[term][3] == '1':
-                circuit[node][3] = '0'
-                break
-            if circuit[term][3] == "U":
-                unknownTerm = True
-        if unknownTerm:
-            if circuit[node][3] == '1':
-                circuit[node][3] = "U"
-        return circuit
-
-    # If the node is an XOR gate output, solve and return the output
-    if circuit[node][0] == "XOR":
-        # Initialize a variable to zero, to count how many 1's in the terms
-        count = 0
-
-        # if there are an odd number of terminals, XOR outputs 1. Otherwise, it should output 0
-        for term in terminals:
-            if circuit[term][3] == '1':
-                count += 1  # For each 1 bit, add one count
-            if circuit[term][3] == "U":
-                circuit[node][3] = "U"
-                return circuit
-
-        # check how many 1's we counted
-        if count % 2 == 1:  # if more than one 1, we know it's going to be 0.
-            circuit[node][3] = '1'
-        else:  # Otherwise, the output is equal to how many 1's there are
-            circuit[node][3] = '0'
-        return circuit
-
-    # If the node is an XNOR gate output, solve and return the output
-    elif circuit[node][0] == "XNOR":
-        # Initialize a variable to zero, to count how many 1's in the terms
-        count = 0
-
-        # if there is a single 1 terminal, XNOR outputs 0. Otherwise, it outputs 1
-        for term in terminals:
-            if circuit[term][3] == '1':
-                count += 1  # For each 1 bit, add one count
-            if circuit[term][3] == "U":
-                circuit[node][3] = "U"
-                return circuit
-
-        # check how many 1's we counted
-        if count % 2 == 1:  # if more than one 1, we know it's going to be 0.
-            circuit[node][3] = '1'
-        else:  # Otherwise, the output is equal to how many 1's there are
-            circuit[node][3] = '0'
-        return circuit
-
-    # Error detection... should not be able to get at this point
-    return circuit[node][0]
-
-# -------------------------------------------------------------------------------------------------------------------- #
-# FUNCTION: calculates the output value for each logic gate using faulty input/outputwires
-def faultGateCalc(circuit, node, lineSpliced):
+# FUNCTION: calculates the output value for each logic gate with a mode to determine wether to use faulty input/outputwires or not
+def gateCalc(circuit, node, lineSpliced, mode=1):
     # terminal will contain all the input wires of this logic gate (node)
     terminals = list(circuit[node][1])  
     accessed = False
 
-    print ("THE FAULT IN CONSIDERATION")
-    print (lineSpliced)
-    
-    # get fault node, terminal, and value
-    if (lineSpliced[1] == 'IN'):
-        fault_wire_node = 'wire_' + lineSpliced[0]
-        fault_wire_term = 'wire_' + lineSpliced[2]
-        fault_wire_val = lineSpliced[4]
-        # print (fault_wire_node)
-        # print (fault_wire_term)
-        # print (fault_wire_val)
-    else:
-        fault_wire_term = 'wire_' + lineSpliced[0]
-        fault_wire_node = fault_wire_term
-        fault_wire_val = lineSpliced[2]
-        # print (fault_wire_node)
-        # print (fault_wire_term)
-        # print ('NEW VALUE')
-        # print (fault_wire_val)
-        # print("")
+    if mode != 1:
+        print ("THE FAULT IN CONSIDERATION")
+        print (lineSpliced)
+        
+        # get fault node, terminal, and value
+        if (lineSpliced[1] == 'IN'):
+            fault_wire_node = 'wire_' + lineSpliced[0]
+            fault_wire_term = 'wire_' + lineSpliced[2]
+            fault_wire_val = lineSpliced[4]
+        else:
+            fault_wire_term = 'wire_' + lineSpliced[0]
+            fault_wire_node = fault_wire_term
+            fault_wire_val = lineSpliced[2]
 
-    # print("THIS IS THE CURRENT NODE")
-    # print(node)
-    # print(lineSpliced[1])
-    # print(terminals)
+        old_val = circuit[fault_wire_term][3]
 
-    # print("THIS IS THE CURRENT TERM")
-    # print(fault_wire_term)
-    # print("OLD VALUE ")
-    old_val = circuit[fault_wire_term][3]
-    # print(old_val)
-
-    # Change gate input terminal wire value to stuck-at value 
-    if node == fault_wire_node and lineSpliced[1] == 'IN':
-        for term in terminals:
-            if (term == fault_wire_term):
-                circuit[term][3] = fault_wire_val
-                accessed = True
-                # print(term + " is ")
-                # print(circuit[term][3])  
-    # Change wire value to stuck-at value
-    elif lineSpliced[1] != 'IN':
-        for term in terminals:
-            if (term == fault_wire_term):
-                accessed = True                
-                circuit[fault_wire_term][3] = fault_wire_val
-                # print(fault_wire_term + " now has ")
-                # print(circuit[fault_wire_term][3])
+        # Change gate input terminal wire value to stuck-at value 
+        if node == fault_wire_node and lineSpliced[1] == 'IN':
+            for term in terminals:
+                if (term == fault_wire_term):
+                    circuit[term][3] = fault_wire_val
+                    accessed = True 
+        # Change wire value to stuck-at value
+        elif lineSpliced[1] != 'IN':
+            for term in terminals:
+                if (term == fault_wire_term):
+                    accessed = True                
+                    circuit[fault_wire_term][3] = fault_wire_val
     
     # If the node is an Inverter gate output, solve and return the output
     if circuit[node][0] == "NOT":
@@ -649,11 +476,7 @@ def basic_sim(circuit, lineSpliced, mode):
 
         if term_has_value:
             circuit[curr][2] = True
-            if mode == 1:
-                circuit = gateCalc(circuit, curr)
-            else:
-                circuit = faultGateCalc(circuit, curr, lineSpliced)
-
+            circuit = gateCalc(circuit, curr, lineSpliced, mode)
 
             # ERROR Detection if LOGIC does not exist
             if isinstance(circuit, str):
@@ -716,7 +539,7 @@ def fault_sim(bench, f_input, f_list, jedi, sith):
     
     detected_fails = len(faults)
     
-    outputFailed.write("total detected faults: " + str(detected_fails) + "\n\n")
+    outputFailed.write("\ntotal detected faults: " + str(detected_fails) + "\n\n")
     outputFailed.write("undetected faults: " + str(size - detected_fails) + "\n")
 
     # Get the undetected faults
